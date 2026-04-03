817ad81c106b98b70e5f1a8eec7bdf3fb60d10b8
jbrongust <jordan.brong.ust@consumerdirect.com>
2026-04-03 11:17:31 -0400

    adds 2 routers

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,15 @@ from dotenv import load_dotenv
 load_dotenv()
 import uvicorn
 from fastapi import FastAPI
-from polls.create import router as polls_router
+from polls.create import router as polls_create_router
+from polls.retrieve import router as polls_retrieve_router
+from polls.vote import router as polls_vote_router
 
 
 app = FastAPI()
-app.include_router(polls_router)
-
+app.include_router(polls_create_router)
+app.include_router(polls_retrieve_router)
+app.include_router(polls_vote_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
