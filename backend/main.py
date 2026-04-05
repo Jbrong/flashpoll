8f7f933bf8851faff067f6d3d61e1421b0fa4c4b
jbrongust <jordan.brong.ust@consumerdirect.com>
2026-04-05 00:03:23 -0400

    new results endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,14 @@ from fastapi import FastAPI
 from polls.create import router as polls_create_router
 from polls.retrieve import router as polls_retrieve_router
 from polls.vote import router as polls_vote_router
+from polls.results import router as polls_results_router
 
 
 app = FastAPI()
 app.include_router(polls_create_router)
 app.include_router(polls_retrieve_router)
 app.include_router(polls_vote_router)
+app.include_router(polls_results_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
