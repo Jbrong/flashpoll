753c1c016a5e7c6419c7cb4e2b6054b096f34665
jbrongust <jordan.brong.ust@consumerdirect.com>
2026-04-06 11:46:36 -0400

    add cors for local dev

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from polls.create import router as polls_create_router
 from polls.retrieve import router as polls_retrieve_router
 from polls.vote import router as polls_vote_router
@@ -9,6 +10,12 @@ from polls.results import router as polls_results_router
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(polls_create_router)
 app.include_router(polls_retrieve_router)
 app.include_router(polls_vote_router)
